e5e61bd6b41158061fe2da5088a7e115b42780f4
RupiahS <charrey.willim@yahoo.com>
2019-11-21 14:52:33 +0700

    aku tampan

---
+++ precache-manifest.6b8fe42f2dbc7e1d00527e916cdab177.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f9499acf3620de3d98",
+    "url": "/css/about.3b3c9ddb.css"
+  },
+  {
+    "revision": "e229c4e0918b06906f32",
+    "url": "/css/chunk-vendors.ec4de8ca.css"
+  },
+  {
+    "revision": "7bead8205f26894ded5455140c33b49d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47f9499acf3620de3d98",
+    "url": "/js/about.af7400b3.js"
+  },
+  {
+    "revision": "3067ba1c0360448145c8",
+    "url": "/js/app.9842c494.js"
+  },
+  {
+    "revision": "e229c4e0918b06906f32",
+    "url": "/js/chunk-vendors.0ddc5fae.js"
+  },
+  {
+    "revision": "9d0d57b9fc5e6b84b4bf66e07b57fa20",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
